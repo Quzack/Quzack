86c89a751dc685521c8fcbdb56b7634389b69a0a
Quzack <85388487+Quzack@users.noreply.github.com>
2021-06-05 13:52:13 +0530

    Update ping.js

--- Commands/cmds/ping.js
+++ Commands/cmds/ping.js
@@ -1,1 +1,13 @@
+module.exports = {
+    name: 'ping',
+    description: "Check bot and player ping",
+    execute(message,args,Discord,client){
+        message.channel.send("Pinging...").then(m => {
+            var ping = m.createdTimestamp - message.createdTimestamp;
+            var botPing = Math.round(client.ws.ping);
+            var PingEm = new Discord.MessageEmbed().setColor("#7289DA").addFields({name: "User ping  ",value: ping + "ms", inline:true}, {name: "  Client ping", value: botPing + "ms", inline:true})
 
+            m.edit(PingEm)
+        })
+    }
+}
